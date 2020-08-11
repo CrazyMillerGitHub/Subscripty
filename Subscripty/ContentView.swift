6ab3ad3760ab108ab3ae65a716ac9519f96b9553
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-08-11 19:49:06 +0300

    -profile init

--- Subscripty/ContentView.swift
+++ Subscripty/ContentView.swift
@@ -6,16 +6,27 @@
 //  Copyright © 2020 Mikhail Borisov. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
+import SPStorkController
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+final class MainViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) { [weak self] in
+            let controller = UIViewController()
+            let transitionDelegate = SPStorkTransitioningDelegate()
+            controller.transitioningDelegate = transitionDelegate
+            controller.modalPresentationStyle = .custom
+            controller.modalPresentationCapturesStatusBarAppearance = true
+            self?.present(controller, animated: true, completion: nil)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
